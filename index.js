d512833ca659492eecb5915388c95e2b52ee75bc
DiegOrtega <nemesisrda@gmail.com>
2018-05-16 18:49:51 -0500

    cambio de configuración de package.json

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ Documentación de MasterIndex
 El objeto jsonDatosInit es la variable constructor con la cual se construye la estructura de datos de los usuarios.
 Este objeto construye el usuario neutral con el cual funciona la plataforma cuando un usuario que no está registrado en el sistema entra. 
 */
-var jsonDatosInit = {nombre:"", ref:false, email:null, external_urls:null, seguidores:null, imagen_url:null, pais:null, access_token:null, track_uri:[], track_uri_ref:null, num:50, danceability:0, energia:0, fundamental:0, amplitud:0, modo:0, dialogo:0, acustica:0, instrumental:0, audiencia:0, positivismo:0, tempo:0, firma_tiempo:0, duracion:0, danceability2:0, energia2:0, fundamental2:0, amplitud2:0, modo2:0, dialogo2:0, acustica2:0, instrumental2:0, audiencia2:0, positivismo2:0, tempo2:0, firma_tiempo2:0, duracion2:0, followers:null, anti_playlist:[], trackid:null ,artist_data:[], track_uri_ref2:[], seedTracks:[], userid:null, seed_shuffled:null, pass:null, pass2:null, mes:null, dia:null, año:null, noticias:null, Userdata:[], mensaje:null, add:null, totalUsers:0, pool:[], playlist:[], popularidadAvg:0}
+var jsonDatosInit = {nombre:"", ref:false, email:null, external_urls:null, seguidores:null, imagen_url:null, pais:null, access_token:null, track_uri:[], track_uri_ref:null, num:50, danceability:0, energia:0, fundamental:0, amplitud:0, modo:0, dialogo:0, acustica:0, instrumental:0, audiencia:0, positivismo:0, tempo:0, firma_tiempo:0, duracion:0, danceability2:0, energia2:0, fundamental2:0, amplitud2:0, modo2:0, dialogo2:0, acustica2:0, instrumental2:0, audiencia2:0, positivismo2:0, tempo2:0, firma_tiempo2:0, duracion2:0, followers:null, anti_playlist:[], trackid:null ,artist_data:[], track_uri_ref2:[], seedTracks:[], userid:null, seed_shuffled:null, pass:null, pass2:null, mes:null, dia:null, año:null, noticias:null, Userdata:[], mensaje:null, add:null, totalUsers:0, pool:[], playlist:[], popularidadAvg:0, usuarios:[]}
 
 var position = 0;
 
@@ -377,6 +377,13 @@ app.get('/perfil', function(request, response, error) {
     
             console.log("objetosGlobales");
             console.log(objetosGlobales);
+            
+            objetosGlobales.forEach(function(item,index){
+                if(index>0){
+                    objetosGlobales[position].usuarios.push(item.nombre)
+                }
+            })
+            
         
             response.render('pages/author-login.ejs', objetosGlobales[position]);
 });
