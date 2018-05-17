45ff74c1b6525897e8cf6108546a32bb94443d16
DiegOrtega <nemesisrda@gmail.com>
2018-05-16 22:44:10 -0500

    mejoras de estructura de backend

--- rutas/perfil.js
+++ rutas/perfil.js
@@ -19,11 +19,17 @@ router.get('/perfil', function(request, response, error) {
 
         objetosGlobales[position].usuarios = []
 
+        function onlyUnique(value, index, self) { 
+            return self.indexOf(value) === index;
+        }
+   
         objetosGlobales.forEach(function(item,index){
             if(index>0 && item.nombre != null){
                 objetosGlobales[position].usuarios.push(item.nombre)
             }
         })
+        
+        var unique = objetosGlobales[position].usuarios.filter( onlyUnique );
 
 
         response.render('pages/author-login.ejs', objetosGlobales[position]);   
