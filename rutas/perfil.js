6f47388462d8c5899f306a2942898f9eb5e1a489
DiegOrtega <nemesisrda@gmail.com>
2018-05-21 19:28:34 -0500

    mejoras de despliegue

--- rutas/perfil.js
+++ rutas/perfil.js
@@ -25,7 +25,7 @@ router.get('/perfil', function(request, response, error) {
         /*Esta parte guarda a los usuarios dentro del pool en la variable usuarios para que sean desplegados en la interfaz*/
         objetosGlobales.forEach(function(item,index){
             if(index!=0 && objetosGlobales[index] != null){
-                objetosGlobales[position].usuarios[index] = [item.nombre,item.imagen_url]
+                objetosGlobales[position].usuarios[index-1] = [item.nombre,item.imagen_url]
                 /*Esta parte filtra a los usuarios repetidos en el sistema de perfil. Ya sea porque están adentro de diferentes perfiles o por cualquier otra razón que dupliqué un usuario*/
                 function onlyUnique(value, index, self) { 
                     return self[0].indexOf(value[0]) === index;
