3ad22d84f095fc4aea1b07c64c8c80a1438f2cc6
Alexis Uriel Guerrero Montoya <Alexis@MacBook-Pro-de-Alexis-2.local>
2018-05-24 15:50:15 -0500

    mejoras interfaz

--- public/script/procesosServidorCliente.js
+++ public/script/procesosServidorCliente.js
@@ -364,7 +364,7 @@ var referenciaBD="noGuardado"
                         // Create the inner div before appending to the body
                         var innerDiv = document.createElement('div');
                         innerDiv.className = 'be-post';
-                        innerDiv.style = ' background-color: rgba(255,255,255,0.9); color:#d5573b; max-height:400px; max-width:250px;';
+                        innerDiv.style = ' background-color: rgba(255,255,255,0.9) !important; color:#d5573b; max-height:400px; max-width:250px;';
 
                         // The variable iDiv is still good... Just append to it.
                         iDiv.appendChild(innerDiv);
@@ -487,7 +487,7 @@ var referenciaBD="noGuardado"
                         // Create the inner div before appending to the body
                         var innerDiv = document.createElement('div');
                         innerDiv.className = 'be-post';
-                        innerDiv.style = ' background-color: rgba(255,255,255,0.9); color:#d5573b; max-height:400px; max-width:250px;';
+                        innerDiv.style = ' background-color: rgba(255,255,255,0.9) !important; color:#d5573b; max-height:400px; max-width:250px;';
 
                         // The variable iDiv is still good... Just append to it.
                         iDiv.appendChild(innerDiv);
@@ -617,7 +617,7 @@ var referenciaBD="noGuardado"
                         // Create the inner div before appending to the body
                         var innerDiv = document.createElement('div');
                         innerDiv.className = 'be-post';
-                        innerDiv.style = ' background-color: rgba(255,255,255,0.9); color:#d5573b; max-height:400px; max-width:250px;';
+                        innerDiv.style = ' background-color: rgba(255,255,255,0.9) !important; color:#d5573b; max-height:400px; max-width:250px;';
 
                         // The variable iDiv is still good... Just append to it.
                         iDiv.appendChild(innerDiv);
@@ -747,7 +747,7 @@ var referenciaBD="noGuardado"
                         // Create the inner div before appending to the body
                         var innerDiv = document.createElement('div');
                         innerDiv.className = 'be-post';
-                        innerDiv.style = ' background-color: rgba(255,255,255,0.9); color:#d5573b; max-height:400px; max-width:250px;';
+                        innerDiv.style = ' background-color: rgba(255,255,255,0.9) !important; color:#d5573b; max-height:400px; max-width:250px;';
 
                         // The variable iDiv is still good... Just append to it.
                         iDiv.appendChild(innerDiv);
@@ -887,7 +887,7 @@ $('.timeFilter').on('click',function(){
             // Create the inner div before appending to the body
             var innerDiv = document.createElement('div');
             innerDiv.className = 'be-post';
-            innerDiv.style = ' background-color: rgba(255,255,255,0.9); color:#d5573b; max-height:400px; max-width:250px;';
+            innerDiv.style = ' background-color: rgba(255,255,255,0.9) !important; color:#d5573b; max-height:400px; max-width:250px;';
 
             // The variable iDiv is still good... Just append to it.
             iDiv.appendChild(innerDiv);
@@ -1044,7 +1044,7 @@ $('.timeFilter').on('click',function(){
             // Create the inner div before appending to the body
             var innerDiv = document.createElement('div');
             innerDiv.className = 'be-post';
-            innerDiv.style = ' background-color: rgba(255,255,255,0.9); color:#d5573b; max-height:400px; max-width:250px;';
+            innerDiv.style = ' background-color: rgba(255,255,255,0.9) !important; color:#d5573b; max-height:400px; max-width:250px;';
 
             // The variable iDiv is still good... Just append to it.
             iDiv.appendChild(innerDiv);
@@ -1120,6 +1120,9 @@ $('.timeFilter').on('click',function(){
 
 })
 
+
+var ua = navigator.userAgent;
+var isiPad = /iPad/i.test(ua) || /iPhone OS 3_1_2/i.test(ua) || /iPhone OS 3_2_2/i.test(ua);
 
 
 
