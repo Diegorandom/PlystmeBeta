9497aef4eeabefded626a2d59101971f8bc8b09b
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2023-06-03 15:09:41 -0600

    deleting node modules (#26)
    * deleting node modules
    
    * deleting node modules 2

--- src/public/script/ajax.js
+++
@@ -1,9 +0,0 @@
- <% if(anti_playlist != undefined){
-        anti_playlist.tracks.forEach(function(records, index){ %>
-                $("#fadeOut").click(function() {
-                    $("#track<%=index%>").fadeOut("slow", function() {
-                        console.log('Faded')
-                    });
-                });
-    <%}) }%>
-               
