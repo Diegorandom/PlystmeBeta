bb8b431d3175f58e298fd2b3a383a0439a6fbc5c
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2023-06-03 15:37:06 -0600

    extract checkUrl out of SM.js

---
+++ src/utils/checkUrl.js
@@ -0,0 +1,14 @@
+const checkUrl = (url) => {
+    console.log('Checando URL -> ', url)
+    return $.get(url)
+        .done(function () {
+            return true
+            // Do something now you know the image exists.
+
+        }).fail(function () {
+            return false
+            // Image doesn't exist - do something else.
+
+        })
+}
+module.exports = checkUrl
