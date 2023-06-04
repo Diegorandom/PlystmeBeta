5d775ca25613a905ed8325871bd2a7be1bcead6e
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2023-06-04 11:34:17 -0600

    Better database transaction for match user, fixed session issue

--- src/database/matchDatabaseUsuario.js
+++ src/database/matchDatabaseUsuario.js
@@ -1,21 +1,12 @@
-const matchDatabaseUsuario = (session, spotifyid) => {
-
+const matchDatabaseUsuario = async (session, spotifyid) => {
     /*Se consulta si el usuario ya existe en la base de datos*/
-    const matchDatabaseUsuario = session
+    const id = await session
         .writeTransaction(tx => tx.run('MATCH (n:usuario) WHERE n.spotifyid={spotifyid} RETURN n', { spotifyid }))
 
-    matchDatabaseUsuario.then(function (checkid_result) {
-        console.log('se realizó la consulta a la base de datos')
+    console.log('id ', id)
 
-        console.log('checkid_result.length:');
-        console.log(checkid_result.records.length)
-        return checkid_result;
-    })
+    return id
 
-    matchDatabaseUsuario.catch(function (err) {
-        console.log(err);
-        return undefined
-    })
 }
 
 module.exports = matchDatabaseUsuario
